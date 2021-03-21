23bd29316dac8dcf56842fbdb57be1f09a1bfb4a
elpatronaco <pau.colome@between.tech>
2021-03-21 10:42:12 +0100

    Deleted scrape console.log

--- src/scripts/amazon/scrape.js
+++ src/scripts/amazon/scrape.js
@@ -5,10 +5,7 @@ module.exports = async ({ link }) =>
     await scrape(link, {
       price: {
         selector: '#price_inside_buybox',
-        convert: x => {
-          console.log(x)
-          return Number(x.replace(/[^0-9,]/g, '').replace(',', '.'))
-        }
+        convert: x => Number(x.replace(/[^0-9,]/g, '').replace(',', '.'))
       },
       stock: {
         selector: '#availability',
